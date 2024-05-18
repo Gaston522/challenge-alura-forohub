82b02e79f5b7415d8f7788eb4a36f498f1553f24
Gaston522 <gastoncoyant@gmail.com>
2024-05-18 15:31:36 -0300

    creacion del proyecto

---
+++ src/test/java/com/gc/ForohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
